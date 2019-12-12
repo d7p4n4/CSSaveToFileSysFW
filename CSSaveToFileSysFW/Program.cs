67638e7789f85fe5de28116707046beac24c7616
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-12 12:40:37 +0100

    1.20191212.2

--- CSSaveToFileSysFW/Program.cs
+++ CSSaveToFileSysFW/Program.cs
@@ -19,23 +19,14 @@ namespace CSSaveToFileSysFW
     {
         private static readonly log4net.ILog _naplo = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        public string conn = ConfigurationManager.AppSettings["connString"];
-        public static SqlConnection sqlConn = new SqlConnection(ConfigurationManager.AppSettings["connString"]);
-        public static SqlConnection sqlConnXML = new SqlConnection(ConfigurationManager.AppSettings["connStringXML"]);
-        public static string TemplateName = ConfigurationManager.AppSettings["TemplateName"];
-        public static string outPath = ConfigurationManager.AppSettings["Path"];
-        public static string outPathProcess = ConfigurationManager.AppSettings["PathProcess"];
-        public static string outPathSuccess = ConfigurationManager.AppSettings["PathSuccess"];
-        public static string outPathError = ConfigurationManager.AppSettings["PathError"];
         static void Main(string[] args)
         {
             try
             {
                 GetXmls getXmls = new GetXmls();
-
-                sqlConn.Open();
-                sqlConnXML.Open();
-                Load();
+                SaveToFileSysFW saveToFileSysFW = new SaveToFileSysFW();
+                
+                saveToFileSysFW.Load();
                 /*
                 List<SerializationObject> xmls = getXmls.GetXmlsMethod(sqlConn, sqlConnXML, TemplateName);
                 foreach(var xml in xmls)
@@ -51,115 +42,5 @@ namespace CSSaveToFileSysFW
 
         }
 
-        public static void WriteOutAc4yObjectHome()
-        {
-            sqlConn.Open();
-            ListInstanceByNameResponse listInstanceByNameResponse =
-                new Ac4yObjectObjectService(sqlConn).ListInstanceByName(
-                    new ListInstanceByNameRequest() { TemplateName = TemplateName }
-                );
-            
-            foreach (var element in listInstanceByNameResponse.Ac4yObjectHomeList)
-            {
-                string xml = serialize(element, typeof(Ac4yObjectHome));
-
-                writeOut(xml, element.SimpledHumanId + "@" + element.TemplateHumanId + "@Ac4yObjectHome", outPath);
-            }
-            
-        }
-
-
-        public static void writeOut(string text, string fileName, string outputPath)
-        {
-            File.WriteAllText(outputPath + fileName + ".xml", text);
-
-        }
-
-        public static string serialize(Object taroltEljaras, Type anyType)
-        {
-            XmlSerializer serializer = new XmlSerializer(anyType);
-            var xml = "";
-
-            using (var writer = new StringWriter())
-            {
-                using (XmlWriter xmlWriter = XmlWriter.Create(writer))
-                {
-                    serializer.Serialize(writer, taroltEljaras);
-                    xml = writer.ToString(); // Your XML
-                }
-            }
-            //System.IO.File.WriteAllText(path, xml);
-
-            return xml;
-        }
-
-        public static void Load()
-        {
-            Ac4yObjectObjectService ac4YObjectObjectService = new Ac4yObjectObjectService(sqlConn);
-
-            try
-            {
-                string[] files =
-                    Directory.GetFiles(outPath, "*.xml", SearchOption.TopDirectoryOnly);
-
-                Console.WriteLine(files.Length);
-
-                foreach (var _file in files)
-                {
-                    string _filename = Path.GetFileNameWithoutExtension(_file);
-                    Console.WriteLine(_filename);
-                    System.IO.File.Move(outPath + _filename + ".xml", outPathProcess + _filename + ".xml");
-
-
-                    string xml = readIn(_filename, outPathProcess);
-
-                    Ac4yObjectHome tabla = (Ac4yObjectHome)deser(xml, typeof(Ac4yObjectHome));
-
-                    SetByNamesResponse response = ac4YObjectObjectService.SetByNames(
-                        new SetByNamesRequest() { TemplateName = TemplateName, Name = tabla.SimpledHumanId }
-                        );
-                    
-                    if(response.Result.Code.Equals("1"))
-                    {
-                        System.IO.File.Move(outPathProcess + _filename + ".xml", outPathSuccess + _filename + ".xml");
-
-                    }
-                    else
-                    {
-                        System.IO.File.Move(outPathProcess + _filename + ".xml", outPathError + _filename + ".xml");
-
-                    }
-                }
-            }
-            catch (Exception _exception)
-            {
-                Console.WriteLine(_exception.Message);
-            }
-        }
-
-        public static Object deser(string xml, Type anyType)
-        {
-            Object result = null;
-
-            XmlSerializer serializer = new XmlSerializer(anyType);
-            using (TextReader reader = new StringReader(xml))
-            {
-                result = serializer.Deserialize(reader);
-            }
-
-            return result;
-        }
-
-        public static string readIn(string fileName, string path)
-        {
-
-            string textFile = path + fileName + ".xml";
-
-            string text = File.ReadAllText(textFile);
-
-            return text;
-
-
-        }
     }
 }
---
+++ CSSaveToFileSysFW/SaveToFileSysFW.cs
@@ -0,0 +1,142 @@
+﻿using CSLibAc4yObjectDBCap;
+using CSLibAc4yObjectObjectService;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace CSSaveToFileSysFW
+{
+    class SaveToFileSysFW
+    {
+
+        public string conn = ConfigurationManager.AppSettings["connString"];
+        public static SqlConnection sqlConn = new SqlConnection(ConfigurationManager.AppSettings["connString"]);
+        public static SqlConnection sqlConnXML = new SqlConnection(ConfigurationManager.AppSettings["connStringXML"]);
+        public static string TemplateName = ConfigurationManager.AppSettings["TemplateName"];
+        public static string outPath = ConfigurationManager.AppSettings["Path"];
+        public static string outPathProcess = ConfigurationManager.AppSettings["PathProcess"];
+        public static string outPathSuccess = ConfigurationManager.AppSettings["PathSuccess"];
+        public static string outPathError = ConfigurationManager.AppSettings["PathError"];
+
+
+        public void WriteOutAc4yObjectHome()
+        {
+            sqlConn.Open();
+            ListInstanceByNameResponse listInstanceByNameResponse =
+                new Ac4yObjectObjectService(sqlConn).ListInstanceByName(
+                    new ListInstanceByNameRequest() { TemplateName = TemplateName }
+                );
+
+            foreach (var element in listInstanceByNameResponse.Ac4yObjectHomeList)
+            {
+                string xml = serialize(element, typeof(Ac4yObjectHome));
+
+                writeOut(xml, element.SimpledHumanId + "@" + element.TemplateHumanId + "@Ac4yObjectHome", outPath);
+            }
+
+        }
+
+
+        public static void writeOut(string text, string fileName, string outputPath)
+        {
+            File.WriteAllText(outputPath + fileName + ".xml", text);
+
+        }
+
+        public string serialize(Object taroltEljaras, Type anyType)
+        {
+            XmlSerializer serializer = new XmlSerializer(anyType);
+            var xml = "";
+
+            using (var writer = new StringWriter())
+            {
+                using (XmlWriter xmlWriter = XmlWriter.Create(writer))
+                {
+                    serializer.Serialize(writer, taroltEljaras);
+                    xml = writer.ToString(); // Your XML
+                }
+            }
+            //System.IO.File.WriteAllText(path, xml);
+
+            return xml;
+        }
+
+        public void Load()
+        {
+
+            sqlConn.Open();
+            Ac4yObjectObjectService ac4YObjectObjectService = new Ac4yObjectObjectService(sqlConn);
+
+            try
+            {
+                string[] files =
+                    Directory.GetFiles(outPath, "*.xml", SearchOption.TopDirectoryOnly);
+
+                Console.WriteLine(files.Length);
+
+                foreach (var _file in files)
+                {
+                    string _filename = Path.GetFileNameWithoutExtension(_file);
+                    Console.WriteLine(_filename);
+                    System.IO.File.Move(outPath + _filename + ".xml", outPathProcess + _filename + ".xml");
+
+
+                    string xml = readIn(_filename, outPathProcess);
+
+                    Ac4yObjectHome tabla = (Ac4yObjectHome)deser(xml, typeof(Ac4yObjectHome));
+
+                    SetByNamesResponse response = ac4YObjectObjectService.SetByNames(
+                        new SetByNamesRequest() { TemplateName = TemplateName, Name = tabla.SimpledHumanId }
+                        );
+
+                    if (response.Result.Code.Equals("1"))
+                    {
+                        System.IO.File.Move(outPathProcess + _filename + ".xml", outPathSuccess + _filename + ".xml");
+
+                    }
+                    else
+                    {
+                        System.IO.File.Move(outPathProcess + _filename + ".xml", outPathError + _filename + ".xml");
+
+                    }
+                }
+            }
+            catch (Exception _exception)
+            {
+                Console.WriteLine(_exception.Message);
+            }
+        }
+
+        public Object deser(string xml, Type anyType)
+        {
+            Object result = null;
+
+            XmlSerializer serializer = new XmlSerializer(anyType);
+            using (TextReader reader = new StringReader(xml))
+            {
+                result = serializer.Deserialize(reader);
+            }
+
+            return result;
+        }
+
+        public string readIn(string fileName, string path)
+        {
+
+            string textFile = path + fileName + ".xml";
+
+            string text = File.ReadAllText(textFile);
+
+            return text;
+
+
+        }
+    }
+}
